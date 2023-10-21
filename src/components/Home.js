c93ff6419bf34e2f4ca8e04da9059e912b6fac08
outerspacesoph <sophlamo97@gmail.com>
2023-10-20 18:41:04 -0600

    groupId const

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,78 +2,81 @@ import {
   SismoConnectButton,
   ClaimType,
   AuthType,
-} from '@sismo-core/sismo-connect-react'
-import { ethers } from 'ethers'
-import ABI from '../contracts/ABI.json'
-import React, { useState } from 'react'
-import HowItWorks from '../assets/HowItWorks.png'
-import Spark from '../assets/spark-logo.svg'
-import Sismo from '../assets/sismo-logo.png'
-import Xmtp from '../assets/xmtp-logo.svg'
+} from "@sismo-core/sismo-connect-react";
+import { ethers } from "ethers";
+import ABI from "../contracts/ABI.json";
+import React, { useState } from "react";
+import HowItWorks from "../assets/HowItWorks.png";
+import Spark from "../assets/spark-logo.svg";
+import Sismo from "../assets/sismo-logo.png";
+import Xmtp from "../assets/xmtp-logo.svg";
 
 const Home = () => {
-  const [connectedAddress, setConnectedAddress] = useState('')
-  const contractAddress = '0xcc03EBBD6F7378aAbD010a8329bfE0e018771480'
-  const [loanAmount, setLoanAmount] = useState(0)
-  const [loanInterest, setLoanInterest] = useState(0)
-  const [showLoan, setShowLoan] = useState(false)
+  const [connectedAddress, setConnectedAddress] = useState("");
+  const contractAddress = "0xcc03EBBD6F7378aAbD010a8329bfE0e018771480";
+  const GITCOIN_PASSPORT_HOLDERS = "0x1cde61966decb8600dfd0749bd371f12";
+  const ROCIFI_CREDIT_HOLDERS = "0xb3ac412738ed399acab21fbda9add42c";
+
+  const [loanAmount, setLoanAmount] = useState(0);
+  const [loanInterest, setLoanInterest] = useState(0);
+  const [showLoan, setShowLoan] = useState(false);
 
   const handleConnectWallet = async () => {
     try {
       if (window.ethereum) {
-        await window.ethereum.request({ method: 'eth_requestAccounts' })
+        await window.ethereum.request({ method: "eth_requestAccounts" });
         const accounts = await window.ethereum.request({
-          method: 'eth_accounts',
-        })
-        setConnectedAddress(accounts[0])
+          method: "eth_accounts",
+        });
+        setConnectedAddress(accounts[0]);
       } else {
         throw new Error(
-          'No Ethereum wallet found in your browser. Please install MetaMask or a compatible wallet.',
-        )
+          "No Ethereum wallet found in your browser. Please install MetaMask or a compatible wallet."
+        );
       }
     } catch (error) {
-      console.error('Error connecting to wallet:', error)
-      throw error
+      console.error("Error connecting to wallet:", error);
+      throw error;
     }
-  }
+  };
 
   const handleLoanEstimate = () => {
-    setLoanAmount(100)
-    setLoanInterest(10)
-    setShowLoan(true)
-  }
+    setLoanAmount(100);
+    setLoanInterest(10);
+    setShowLoan(true);
+  };
 
-  const handleGetLoan = () => {}
+  const handleGetLoan = () => {};
 
   const callSismoContract = async (response) => {
     try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(contractAddress, ABI, provider)
-      console.log('Contract: ', contract)
-      const result = await contract.doSomethingUsingSismoConnect(response) // Replace with the function name you want to call
-      console.log('Function result:', result)
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(contractAddress, ABI, provider);
+      console.log("Contract: ", contract);
+      const result = await contract.doSomethingUsingSismoConnect(response); // Replace with the function name you want to call
+      console.log("Function result:", result);
     } catch (error) {
-      console.error('Error calling smart contract function:', error)
+      console.error("Error calling smart contract function:", error);
     }
-  }
+  };
 
   const config = {
-    appId: '0x70fa08c440c103a75df7bb076c84b99f',
+    appId: "0x70fa08c440c103a75df7bb076c84b99f",
     displayRawResponse: true, // this enables you to get access directly to the
     // Sismo Connect Response in the vault instead of redirecting back to the app
-  }
+  };
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <header className="App-header">
         <div style={{ paddingLeft: 20 }}>
           <span
-            style={{ fontSize: 'larger', color: '#114084', fontWeight: 'bold' }}
+            style={{ fontSize: "larger", color: "#114084", fontWeight: "bold" }}
           >
             ZK
           </span>
           <span
-            style={{ fontSize: 'larger', color: '#3466AA', fontWeight: 'bold' }}
+            style={{ fontSize: "larger", color: "#3466AA", fontWeight: "bold" }}
           >
             redit
           </span>
@@ -83,21 +86,21 @@ const Home = () => {
             <div>
               <span
                 style={{
-                  color: '#82B7DC',
-                  fontSize: 'medium',
-                  fontWeight: 'bold',
+                  color: "#82B7DC",
+                  fontSize: "medium",
+                  fontWeight: "bold",
                 }}
               >
-                Connected to:{' '}
+                Connected to:{" "}
               </span>
               <span
                 style={{
-                  color: 'black',
-                  fontSize: 'large',
-                  backgroundColor: '#82B7DC',
-                  borderRadius: '10px',
-                  padding: '4px 8px',
-                  color: 'white',
+                  color: "black",
+                  fontSize: "large",
+                  backgroundColor: "#82B7DC",
+                  borderRadius: "10px",
+                  padding: "4px 8px",
+                  color: "white",
                 }}
               >
                 {connectedAddress}
@@ -112,10 +115,10 @@ const Home = () => {
       </header>
       <div
         style={{
-          flexDirection: 'column',
-          display: 'flex',
-          width: '70vw',
-          marginTop: '10vh',
+          flexDirection: "column",
+          display: "flex",
+          width: "70vw",
+          marginTop: "10vh",
         }}
       >
         <div className="intro">
@@ -139,10 +142,10 @@ const Home = () => {
           <h4>Powered by:</h4>
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignContent: 'center',
-              justifyContent: 'center',
+              display: "flex",
+              flexDirection: "row",
+              alignContent: "center",
+              justifyContent: "center",
             }}
           >
             <a href="https://www.sismo.io/">
@@ -174,26 +177,28 @@ const Home = () => {
       </div>
       <div
         style={{
-          position: 'fixed',
+          position: "fixed",
           bottom: 0,
           right: 0,
-          width: '30vw',
-          height: '90vh',
+          width: "30vw",
+          height: "90vh",
         }}
       >
         <div
           style={{
-            flexDirection: 'column',
-            display: 'flex',
-            padding: '30px',
-            backgroundColor: '#F1F1F1',
-            height: 'inherit',
+            flexDirection: "column",
+            display: "flex",
+            padding: "30px",
+            backgroundColor: "#F1F1F1",
+            height: "inherit",
           }}
         >
-          <div className="content-section" style={{color: "#82B7DC"}}>
+          <div className="content-section" style={{ color: "#82B7DC" }}>
             <h2>GET STARTED</h2>
-            <p>Quickly get started by signing in with sismo to get the required information for generating
-              the trust score and get an estimate of the eligible loan amount.
+            <p>
+              Quickly get started by signing in with sismo to get the required
+              information for generating the trust score and get an estimate of
+              the eligible loan amount.
             </p>
           </div>
           <div className="content-section">
@@ -216,7 +221,7 @@ const Home = () => {
                   // Data Group members          = Gitcoin Passport Holders
                   // value for each group member = Gitcoin Passport Score
                   // request user to prove membership in the group with value > 20
-                  groupId: 'GITCOIN_PASSPORT_HOLDERS',
+                  groupId: GITCOIN_PASSPORT_HOLDERS,
                   value: 20,
                   claimType: ClaimType.GTE,
                 },
@@ -224,7 +229,7 @@ const Home = () => {
                   // claim Rocifi Credit Score Data Group membership: https://factory.sismo.io/groups-explorer?search=0xb3ac412738ed399acab21fbda9add42c
                   // Data Group members          = eth addresses scored by Rocifi
                   // value for each group member = Rocifi Credit Score 1-10
-                  groupId: 'ROCIFI_CREDIT_HOLDERS',
+                  groupId: ROCIFI_CREDIT_HOLDERS,
                   isSelectableByUser: true, // can reveal more than 1 if they want
                   isOptional: true,
                 },
@@ -235,8 +240,8 @@ const Home = () => {
               }}
               // retrieve the Sismo Connect Response from the user's Sismo data vault
               onResponseBytes={(response) => {
-                console.log('Call Sismo Contract.')
-                callSismoContract(response)
+                console.log("Call Sismo Contract.");
+                callSismoContract(response);
                 //    call your contract/backend with the response as bytes
               }}
             />
@@ -244,7 +249,14 @@ const Home = () => {
 
           <div className="content-section">
             {showLoan ? (
-              <div style={{ flexDirection: 'column', display: 'flex', color: "#82B7DC", fontWeight: "bold" }}>
+              <div
+                style={{
+                  flexDirection: "column",
+                  display: "flex",
+                  color: "#82B7DC",
+                  fontWeight: "bold",
+                }}
+              >
                 <span className="loan-info">Loan Amount: {loanAmount}</span>
                 <span className="loan-info">
                   Interest rate: {loanInterest}% p.a.
@@ -265,7 +277,7 @@ const Home = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
